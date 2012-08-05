f8f2056d6210ef047dd07ff4e9c5a38ec6b78122
William Roe <willroe@gmail.com>
2012-08-05 02:05:49 +0100

    Refactor out movement functions from the main game loop

--- src/slick_game/simple_game.clj
+++ src/slick_game/simple_game.clj
@@ -27,9 +27,59 @@
 (def float*
   (comp float *))
 
-(defn isInput
-  [key input]
-  (.isKeyDown input key))
+(defn go-left
+  [gc delta]
+  (.rotate @plane (float (* -0.2 delta))))
+
+(defn go-right
+  [gc delta]
+  (.rotate @plane (float (* 0.2 delta))))
+
+(defn go-forward
+  [gc delta]
+  (let [hip (* 0.4 delta)
+        rotation (. @plane getRotation)]
+    (dosync
+     (alter plane-x float+
+            (* hip (Math/sin (Math/toRadians rotation))))
+     (alter plane-y float-
+            (* hip (Math/cos (Math/toRadians rotation)))))))
+
+(defn zoom-in
+  [gc delta]
+  (dosync
+   (alter scale float+
+          (if (>= @scale 5.0)
+            0
+            0.1))
+   (.setCenterOfRotation @plane
+                         (middle-scale (.getWidth @plane) @scale)
+                         (middle-scale (.getHeight @plane) @scale))))
+
+(defn zoom-out
+  [gc delta]
+  (dosync
+   (alter scale float-
+          (if (<= @scale 1.0)
+            0
+            0.1))
+   (.setCenterOfRotation @plane
+                         (middle-scale (.getWidth @plane) @scale)
+                         (middle-scale (.getHeight @plane) @scale))))
+
+(def keyboard-mappings
+  {Input/KEY_A go-left
+   Input/KEY_D go-right
+   Input/KEY_W go-forward
+   Input/KEY_2 zoom-in
+   Input/KEY_1 zoom-out})
+
+(defn input-event
+  [gc delta]
+  (let [input (.getInput gc)]
+    (doseq [[key fun] (filter (fn [[key fun]] (.isKeyDown input key))
+                              keyboard-mappings)]
+      (fun gc delta))))
 
 (defn -main
   []
@@ -39,43 +89,7 @@
                                     (ref-set land (Image. "data/land.jpg"))
                                     (ref-set plane (Image. "data/plane.png"))))
                                  (update [gc delta]
-                                   (let [input (.getInput gc)]
-                                     (condp isInput input
-
-                                       Input/KEY_A
-                                       (.rotate @plane (float (* -0.2 delta)))
-
-                                       Input/KEY_D
-                                       (.rotate @plane (float (* 0.2 delta)))
-
-                                       Input/KEY_W
-                                       (let [hip (* 0.4 delta)
-                                             rotation (. @plane getRotation)]
-                                         (dosync
-                                          (alter plane-x float+
-                                                 (* hip (Math/sin (Math/toRadians rotation))))
-                                          (alter plane-y float-
-                                                 (* hip (Math/cos (Math/toRadians rotation))))))
-
-                                       Input/KEY_2
-                                       (dosync
-                                        (alter scale float+
-                                               (if (>= @scale 5.0)
-                                                 0
-                                                 0.1))
-                                        (.setCenterOfRotation @plane
-                                                              (middle-scale (.getWidth @plane) @scale)
-                                                              (middle-scale (.getHeight @plane) @scale)))
-
-                                       Input/KEY_1
-                                       (dosync
-                                        (alter scale float-
-                                               (if (<= @scale 1.0)
-                                                 0
-                                                 0.1))
-                                        (.setCenterOfRotation @plane
-                                                              (middle-scale (.getWidth @plane) @scale)
-                                                              (middle-scale (.getHeight @plane) @scale))))))
+                                   (input-event gc delta))
                                  (render [gc g]
                                    (.draw @land 0 0)
                                    (.draw @plane @plane-x @plane-y @scale))))]
