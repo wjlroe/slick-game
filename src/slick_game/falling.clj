4372f66b6ba6ddbb68cbe9486e366eecbed731c8
William Roe <willroe@gmail.com>
2012-08-09 23:27:32 +0100

    Make random shape appear at top of screen and move down

--- src/slick_game/falling.clj
+++ src/slick_game/falling.clj
@@ -9,8 +9,13 @@
     Image
     Input]))
 
+(def ^:dynamic *display-width* 800)
+(def ^:dynamic *display-height* 600)
+(def ^:dynamic *falling-speed* 500)
+
 (def falling-squares (ref []))
 (def last-move (ref 0))
+(def delta-countdown (ref 0))
 
 (defn basic-square
   []
@@ -20,79 +25,93 @@
 (def sq-height 30)
 
 (def shapes
-  [{:name "O"
-    :config
-    [[0 0]
-     [sq-height sq-width]
-     [0 sq-height]
-     [sq-width 0]]}
-   {:name "J"
-    :config
-    [[0 (* 2 sq-height)]
-     [sq-width 0]
-     [sq-width sq-height]
-     [sq-width (* 2 sq-height)]]}
-   {:name "I"
-    :config
-    [[0 0]
-     [0 sq-height]
-     [0 (* 2 sq-height)]
-     [0 (* 3 sq-height)]]}
-   {:name "L"
-    :config
-    [[0 0]
-     [0 sq-height]
-     [0 (* 2 sq-height)]
-     [sq-width (* 2 sq-height)]]}
-   {:name "S"
-    :config
-    [[0 sq-height]
-     [sq-width 0]
-     [sq-width sq-height]
-     [(* 2 sq-width) 0]]}
-   {:name "Z"
-    :config
-    [[0 0]
-     [sq-width 0]
-     [sq-width sq-height]
-     [(* 2 sq-width) sq-height]]}
-   {:name "T"
-    :config
-    [[0 0]
-     [sq-width 0]
-     [sq-width sq-height]
-     [(* 2 sq-width) 0]]}])
+  {:o
+   [[0 0]
+    [sq-height sq-width]
+    [0 sq-height]
+    [sq-width 0]]
+   :j
+   [[0 (* 2 sq-height)]
+    [sq-width 0]
+    [sq-width sq-height]
+    [sq-width (* 2 sq-height)]]
+   :i
+   [[0 0]
+    [0 sq-height]
+    [0 (* 2 sq-height)]
+    [0 (* 3 sq-height)]]
+   :l
+   [[0 0]
+    [0 sq-height]
+    [0 (* 2 sq-height)]
+    [sq-width (* 2 sq-height)]]
+   :s
+   [[0 sq-height]
+    [sq-width 0]
+    [sq-width sq-height]
+    [(* 2 sq-width) 0]]
+   :z
+   [[0 0]
+    [sq-width 0]
+    [sq-width sq-height]
+    [(* 2 sq-width) sq-height]]
+   :t
+   [[0 0]
+    [sq-width 0]
+    [sq-width sq-height]
+    [(* 2 sq-width) 0]]})
+
+(defn get-shape
+  "Get the co-ordinates of a shape. Either returns the required shape or a random shape."
+  ([]
+     (get-shape (rand-nth (keys shapes))))
+  ([shape-name]
+     (get shapes shape-name)))
 
 (defn make-shape
-  "Just makes a square atm"
-  [origin name]
+  "Make a shape. Either return the shape required or a random one."
+  [origin & shape-name]
   {:origin origin
    :graphic (basic-square)
    :blocks
-   (first (filter #(= name (:name %)) shapes))})
+   (apply get-shape shape-name)})
+
+(defn shift-down
+  [shape]
+  (assoc shape :origin (assoc (:origin shape) :y
+                              (+ sq-height
+                                 (:y (:origin shape))))))
 
 (defn render-square
   [gc g {:keys [origin graphic blocks]}]
-  (doseq [[x y] (:config blocks)]
+  (doseq [[x y] blocks]
     (.draw graphic
            (+ (:x origin) x)
            (+ (:y origin) y))))
+
+(defn midpoint
+  []
+  (- (/ *display-width* 2)
+     (* 2 sq-width)))
 
 (defn init-world
   "Nothing to do maybe?"
   [gc]
   (dosync
-   (ref-set falling-squares [(make-shape {:x 0 :y 0} "O")
-                             (make-shape {:x 70 :y 0} "J")
-                             (make-shape {:x 140 :y 0} "I")
-                             (make-shape {:x 210 :y 0} "L")
-                             (make-shape {:x 280 :y 0} "S")
-                             (make-shape {:x 400 :y 0} "Z")
-                             (make-shape {:x 500 :y 0} "T")])))
+   (ref-set falling-squares [(make-shape {:x (midpoint) :y 0})])))
+
+(defn move-pieces-down
+  []
+  (ref-set falling-squares (map shift-down @falling-squares)))
 
 (defn input-event
   "Shove new shape in the world every x seconds"
-  [gc delta])
+  [gc delta]
+  (dosync
+    (alter delta-countdown - delta)
+    (when (< @delta-countdown 0)
+      (move-pieces-down)
+      (ref-set delta-countdown *falling-speed*))))
 
 (defn render-world
   "Loop through all placed shapes, rendering them"
@@ -110,5 +129,5 @@
                                  (render [gc g]
                                    (render-world gc g))))]
     (doto app
-      (.setDisplayMode 800 600 false)
+      (.setDisplayMode *display-width* *display-height* false)
       (.start))))
