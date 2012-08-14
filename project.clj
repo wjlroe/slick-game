42c90fae0dd680f6f9f352ea168e79ad5dfe01fa
William Roe <willroe@gmail.com>
2012-08-14 03:32:01 +0100

    Remove all the Slick code

--- project.clj
+++ project.clj
@@ -1,19 +1,7 @@
-(defn get-native-lib-path
-  [os-name]
-  (cond
-   (re-find #"(?i)mac" os-name) "native/macosx"
-   (re-find #"(?i)win" os-name) "native/windows"
-   (re-find #"(?i)linux" os-name) "native/linux"
-   (re-find #"(?i)solaris" os-name) "native/solaris"))
-
 (defproject slick-game "0.1.0-SNAPSHOT"
-  :description "A game written in Clojure, using Slick as a first attempt at 2D game programming"
+  :description "A game written in Clojure, as a first attempt at 2D game programming"
   :url "https://github.com/wjlroe/slick-game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [slick/slick "274"]]
-  :repositories {"slick" "http://slick.cokeandcode.com/mavenrepo/"}
-  :jvm-opts [~(str "-Djava.library.path=" (get-native-lib-path (System/getProperty "os.name")))]
-  :main slick-game.controller
-  :aot [slick-game.controller])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main slick-game.controller)
--- src/slick_game/falling.clj
+++ src/slick_game/falling.clj
@@ -1,147 +1,138 @@
-(ns slick-game.falling
-  (:import
-   [org.newdawn.slick
-    AppGameContainer
-    BasicGame
-    GameContainer
-    Graphics
-    SlickException
-    Image
-    Input]
-   [org.newdawn.slick.imageout
-    ImageOut]))
+(ns slick-game.falling)
 
-(def ^:dynamic *display-width* 800)
-(def ^:dynamic *display-height* 600)
-(def ^:dynamic *falling-speed* 500)
+(comment
+  (def ^:dynamic *display-width* 800)
+  (def ^:dynamic *display-height* 600)
+  (def ^:dynamic *falling-speed* 500)
 
-(def falling-squares (ref []))
-(def last-move (ref 0))
-(def delta-countdown (ref 0))
-(def capturenum (ref 0))
+  (def falling-squares (ref []))
+  (def last-move (ref 0))
+  (def delta-countdown (ref 0))
+  (def capturenum (ref 0))
 
-(defn basic-square
-  []
-  (Image. "data/beige_sq.jpg"))
+  (defn basic-square
+    []
+    (Image. "data/beige_sq.jpg"))
 
-(def sq-width 30)
-(def sq-height 30)
+  (def sq-width 30)
+  (def sq-height 30)
 
-(def shapes
-  {:o
-   [[0 0]
-    [sq-height sq-width]
-    [0 sq-height]
-    [sq-width 0]]
-   :j
-   [[0 (* 2 sq-height)]
-    [sq-width 0]
-    [sq-width sq-height]
-    [sq-width (* 2 sq-height)]]
-   :i
-   [[0 0]
-    [0 sq-height]
-    [0 (* 2 sq-height)]
-    [0 (* 3 sq-height)]]
-   :l
-   [[0 0]
-    [0 sq-height]
-    [0 (* 2 sq-height)]
-    [sq-width (* 2 sq-height)]]
-   :s
-   [[0 sq-height]
-    [sq-width 0]
-    [sq-width sq-height]
-    [(* 2 sq-width) 0]]
-   :z
-   [[0 0]
-    [sq-width 0]
-    [sq-width sq-height]
-    [(* 2 sq-width) sq-height]]
-   :t
-   [[0 0]
-    [sq-width 0]
-    [sq-width sq-height]
-    [(* 2 sq-width) 0]]})
+  (def shapes
+    {:o
+     [[0 0]
+      [sq-height sq-width]
+      [0 sq-height]
+      [sq-width 0]]
+     :j
+     [[0 (* 2 sq-height)]
+      [sq-width 0]
+      [sq-width sq-height]
+      [sq-width (* 2 sq-height)]]
+     :i
+     [[0 0]
+      [0 sq-height]
+      [0 (* 2 sq-height)]
+      [0 (* 3 sq-height)]]
+     :l
+     [[0 0]
+      [0 sq-height]
+      [0 (* 2 sq-height)]
+      [sq-width (* 2 sq-height)]]
+     :s
+     [[0 sq-height]
+      [sq-width 0]
+      [sq-width sq-height]
+      [(* 2 sq-width) 0]]
+     :z
+     [[0 0]
+      [sq-width 0]
+      [sq-width sq-height]
+      [(* 2 sq-width) sq-height]]
+     :t
+     [[0 0]
+      [sq-width 0]
+      [sq-width sq-height]
+      [(* 2 sq-width) 0]]})
 
-(defn get-shape
-  "Get the co-ordinates of a shape. Either returns the required shape or a random shape."
-  ([]
-     (get-shape (rand-nth (keys shapes))))
-  ([shape-name]
-     (get shapes shape-name)))
+  (defn get-shape
+    "Get the co-ordinates of a shape. Either returns the required shape or a random shape."
+    ([]
+       (get-shape (rand-nth (keys shapes))))
+    ([shape-name]
+       (get shapes shape-name)))
 
-(defn make-shape
-  "Make a shape. Either return the shape required or a random one."
-  [origin & shape-name]
-  {:origin origin
-   :graphic (basic-square)
-   :blocks
-   (apply get-shape shape-name)})
+  (defn make-shape
+    "Make a shape. Either return the shape required or a random one."
+    [origin & shape-name]
+    {:origin origin
+     :graphic (basic-square)
+     :blocks
+     (apply get-shape shape-name)})
 
-(defn shift-down
-  [shape]
-  (assoc shape :origin (assoc (:origin shape) :y
-                              (+ sq-height
-                                 (:y (:origin shape))))))
+  (defn shift-down
+    [shape]
+    (assoc shape :origin (assoc (:origin shape) :y
+                                (+ sq-height
+                                   (:y (:origin shape))))))
 
-(defn render-square
-  [gc g {:keys [origin graphic blocks]}]
-  (doseq [[x y] blocks]
-    (.draw graphic
-           (+ (:x origin) x)
-           (+ (:y origin) y))))
+  (defn render-square
+    [gc g {:keys [origin graphic blocks]}]
+    (doseq [[x y] blocks]
+      (.draw graphic
+             (+ (:x origin) x)
+             (+ (:y origin) y))))
 
-(defn midpoint
-  []
-  (- (/ *display-width* 2)
-     (* 2 sq-width)))
+  (defn midpoint
+    []
+    (- (/ *display-width* 2)
+       (* 2 sq-width)))
 
-(defn init-world
-  "Nothing to do maybe?"
-  [gc]
-  (dosync
-   (ref-set falling-squares [(make-shape {:x (midpoint) :y 0})])))
+  (defn init-world
+    "Nothing to do maybe?"
+    [gc]
+    (dosync
+     (ref-set falling-squares [(make-shape {:x (midpoint) :y 0})])))
 
-(defn move-pieces-down
-  []
-  (ref-set falling-squares (map shift-down @falling-squares)))
+  (defn move-pieces-down
+    []
+    (ref-set falling-squares (map shift-down @falling-squares)))
 
-(defn grab-screen
-  [gc]
-  (let [screen (Image. *display-width* *display-height*)]
-    (-> gc
-        (.getGraphics)
-        (.copyArea screen 0 0))
-    (ImageOut/write screen ImageOut/PNG
-                    (format "grabs/screen%03d.png"
-                            (dosync (alter capturenum + 1))))))
+  (defn grab-screen
+    [gc]
+    (let [screen (Image. *display-width* *display-height*)]
+      (-> gc
+          (.getGraphics)
+          (.copyArea screen 0 0))
+      (ImageOut/write screen ImageOut/PNG
+                      (format "grabs/screen%03d.png"
+                              (dosync (alter capturenum + 1))))))
 
-(defn input-event
-  "Shove new shape in the world every x seconds"
-  [gc delta]
-  (dosync
-    (alter delta-countdown - delta)
-    (when (< @delta-countdown 0)
-      (move-pieces-down)
-      (grab-screen gc)
-      (ref-set delta-countdown *falling-speed*))))
+  (defn input-event
+    "Shove new shape in the world every x seconds"
+    [gc delta]
+    (dosync
+     (alter delta-countdown - delta)
+     (when (< @delta-countdown 0)
+       (move-pieces-down)
+       (grab-screen gc)
+       (ref-set delta-countdown *falling-speed*))))
 
-(defn render-world
-  "Loop through all placed shapes, rendering them"
-  [gc g]
-  (doseq [shape @falling-squares]
-    (render-square gc g shape)))
+  (defn render-world
+    "Loop through all placed shapes, rendering them"
+    [gc g]
+    (doseq [shape @falling-squares]
+      (render-square gc g shape)))
 
-(defn -main
-  []
-  (let [app (AppGameContainer. (proxy [BasicGame] ["Falling blocks"]
-                                 (init [gc]
-                                   (init-world gc))
-                                 (update [gc delta]
-                                   (input-event gc delta))
-                                 (render [gc g]
-                                   (render-world gc g))))]
-    (doto app
-      (.setDisplayMode *display-width* *display-height* false)
-      (.start))))
+  (defn -main
+    []
+    (let [app (AppGameContainer. (proxy [BasicGame] ["Falling blocks"]
+                                   (init [gc]
+                                     (init-world gc))
+                                   (update [gc delta]
+                                     (input-event gc delta))
+                                   (render [gc g]
+                                     (render-world gc g))))]
+      (doto app
+        (.setDisplayMode *display-width* *display-height* false)
+        (.start))))
+)
